bea010e82f5cd6b034551cef43a97776442f9163
Kristin <kristindragos@gmail.com>
2016-12-04 21:24:48 -0700

    Initial commit

---
+++ Assets/vrnd-course3-starter-project-002/Assets/Udacity/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+    // Create a boolean value called "locked" that can be checked in Update() 
+
+    void Update() {
+        // If the door is unlocked and it is not fully raised
+            // Animate the door raising up
+    }
+
+    public void Unlock()
+    {
+        // You'll need to set "locked" to true here
+    }
+}
---
+++ Assets/vrnd-course3-starter-project-002/Assets/Udacity/Scripts/Key.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour 
+{
+    //Create a reference to the KeyPoofPrefab and Door
+
+	void Update()
+	{
+		//Bonus: Key Animation
+	}
+
+	public void OnKeyClicked()
+	{
+        // Instatiate the KeyPoof Prefab where this key is located
+        // Make sure the poof animates vertically
+        // Call the Unlock() method on the Door
+        // Destroy the key. Check the Unity documentation on how to use Destroy
+    }
+
+}
