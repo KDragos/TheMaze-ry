c42ba40082efe286d1eb3f63d6d5c1132977167c
Kristin <kristindragos@gmail.com>
2016-12-06 20:49:51 -0700

    Added keySpawner.
    Feature: Added a key spawner that will randomly spawn a key in one of the spaces designated as a key spawn point.

---
+++ Assets/KeySpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeySpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoints;
+	public GameObject key;
+
+	// Use this for initialization
+	void Start () {
+		SpawnKey();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos() {
+		for (int i = 0; i < spawnPoints.Length; i++) {
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(spawnPoints[i].transform.position, 1);
+		}
+    }
+
+    private void SpawnKey ()
+	{
+		int randomSpawnPont = Random.Range(0, spawnPoints.Length);
+		Debug.Log("Spawning key at point: " + randomSpawnPont.ToString());
+		Instantiate(key, spawnPoints[randomSpawnPont].transform.position, Quaternion.identity);
+	}
+
+}
