447e6a54d3d02f247cc09b5ff177119b9aa43d4c
Kristin <kristindragos@gmail.com>
2016-12-15 13:44:31 -0800

    Updated the door animation
    FEATURE: Play the annimation for the door opening when users click on the door and have the key.
    FIX: Modified the animation to prevent the door from moving through the pillar. It should now stop at a more natural point.

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -16,34 +16,22 @@ public class Door : MonoBehaviour
     void Start ()
 	{
 		locked = true;
-//		audioSource = FindObjectOfType<AudioSource>();
-//		Debug.Log(audioSource.enabled);
-//		audioSource.clip = doorLocked;
-//		audioSource.Play();
-
 	}
 
     void Update ()
 	{
-		// If the door is unlocked and it is not fully raised
-		if (locked) {
-			// Animate the door raising up
 
-		}
     }
 
     public void Unlock()
     {
         locked = false;
-        Debug.Log("Door is now unlocked.");
-		Debug.Log(GetComponent<AudioSource>().ToString());
         audioSource.clip = doorUnlocked;
         audioSource.Play();
 
         Text message = (Text) Instantiate(messageText, helpMessage.transform, false);
         message.text = "You've unlocked the door. Now you must find it to win.";
 		Destroy(message, 5f);
-        Debug.Log("Created message.");
     }
 
     public void Open ()
@@ -52,9 +40,9 @@ public class Door : MonoBehaviour
 			Text message = (Text) Instantiate(messageText, helpMessage.transform, false);
         	message.text = "This door is locked. There must be some way to open it.";
         	Destroy(message, 5f);
-
 		} else {
 			Text message = (Text) Instantiate(messageText, helpMessage.transform, false);
+			gameObject.GetComponent<Animator>().SetBool("HasKey", true);
         	message.text = "Congratulations!";
 			Destroy(message, 5f);
 		}
